1ad9bdd65b8aa847bde56b3bbf61c58707776956
vipul <vbhilare@smu.edu>
2018-11-11 22:27:19 -0600

    website modified

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -7,48 +7,62 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <title>Pushkar</title>
 
 </head>
-<body>
+<body background="https://centerstudioarchitecture.com/wp/wp-content/uploads/2017/02/109WParrish_After_5.jpg">
 <div id="header">
 
 </div>
 <div class="container">
         <div class="row">
-                <legend>Please fill the following form to book a  tick                                                                                                  
-et</legend>
+           <h3>Please fill the following form to book a  ticket</h3>
         </div>
         <div class="row">
+
+
+
+
         <form action="/welcome/book" method="POST">
-          <div class="form-group">
+     
+
+
+        <center><div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
                 <input type="email" name="email" class="form-control"
-id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ente                                                                                                  
-r
-email">
-          </div>
-          <div class="form-group">
-                <label for="name">Name</label>
-                <input type="text" name="Name" id="Name">
-          </div>
-          <div class="form-group">
-                <label for="DOB">Date of Birth</label>
-                <input type="text" id="datepicker" name="DOB">
-          </div>
-          <div class="form-group">
-                <label for="address">Address</label>
-                <input type="text" id="address" name="Address">
-          </div>
-          <div class="form-group">
+id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enteremail">
+          </div> </center>
+
+
+
+         <center>  <div class="form-group">
+                      <label for="name">Name</label>
+                      <input type="text" name="Name" id="Name">
+                   </div> </center>
+
+
+
+         <center>  <div class="form-group">
+                      <label for="DOB">Date of Birth</label>
+                      <input type="text" id="datepicker" name="DOB">
+                   </div> </center>
+          
+
+
+         <center> <div class="form-group">
+                     <label for="address">Address</label>
+                     <input type="text" id="address" name="Address">
+                  </div>  </center>
+
+
+        <center>  <div class="form-group">
                 <label for="Dance_Form">Dance Type</label>
-                <select class="form-control" name="Dance Type" id="typ                                                                                                  
-e">
+                <select class="form-control" name="Dance Type" id="type">
                   <option>Hip-Hop</option>
                   <option>Jazz</option>
                   <option>Classical</option>
                   <option>Bollywood</option>
                 </select>
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button                                                                                                  
->
+          </div> </center>
+         
+            <button type="submit" class="btn btn-primary">Submit</button>
 </form>
         </div>
         </div>
