36c52a4922894561d8e15a3dae1c01e6af3908f6
pushmessi10 <pushkarmadrap@gmail.com>
2018-11-11 21:03:05 -0600

    Website edited

--- application/views/welcome_result.php
+++ application/views/welcome_result.php
@@ -3,45 +3,47 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Pushkar</title>
+        <meta charset="utf-8">
+        <title>VPV</title>
 </head>
 <body>
 <div id="header">
-	
+
 </div>
 <div class="container">
-	<div class="row">
-		<p>Thank you for the registration. Here is the information that you entered.</p>
-	</div>
-	<div class="row">
-		<table class="table">
-			<tr>
-				<td><em>Name</em></td>
-				<td><?php echo $Name ?></td>
-			</tr>
-			<tr>
-				<td><em>Phone No</em></td>
-				<td><?php echo $Phone_No ?></td>
-			</tr>
-			<tr>
-				<td><em>Address</em></td>
-				<td><?php echo $Address ?></td>
-			</tr>
-            <tr>
-                <td><em>date</em></td>
-                <td><?php echo $Date of Birth ?></td>
-            </tr>
-            <tr>
-                <td><em>dance_type</em></td>
-                <td><?php echo $Dance_type ?></td>
-            </tr>
-      </table>
-	</div>
-	<div class="row">
-		<button type="button" id="back" class="btn btn-primary">Make new registration</button>
-	</div>
+        <div class="row">
+                <p>Thank you for the registration. Here are your registration details.</p>
+        </div>
+        <div class="row">
+                <table class="table">
+                        <tr>
+                                <td><em>Email</em></td>
+                                <td><?php echo $email ?></td>
+                        </tr>
+                        <tr>
+                                <td><em>Name</em></td>
+                                <td><?php echo $Name ?></td>
+                        </tr>
+                        <tr>
+                                <td><em>Date of Birth</em></td>
+                                <td><?php echo $DOB ?></td>
+                        </tr>
+                        <tr>
+                                <td><em>Address</em></td>
+                                <td><?php echo $Address ?></td>
+                        </tr>
+                        <tr>
+                                <td><em>Dance Type</em></td>
+                                <td><?php echo $Dance Type ?></td>
+                        </tr>
+                </table>
+        </div>
+        <div class="row">
+                <button type="button" id="back" class="btn
+btn-primary">Make new booking</button>
+        </div>
 </div>
 
 </body>
 </html>
+
--- bookings.sql
+++ bookings.sql
@@ -1,9 +1,8 @@
 CREATE TABLE bookings (
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name varchar(30) null,
-Phone_no INT(12) null,
-Address VARCHAR(30) null,
-Email VARCHAR(30) null,
+email varchar(30) null,
+Name varchar(30) null,
 DOB date null,
-Dance_type VARCHAR(30) null
+Address varchar(30) null,
+Dance Type varchar(30) null
 )ENGINE=innodb;
