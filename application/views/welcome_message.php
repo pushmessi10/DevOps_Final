512d6502be90cfe8dadd8cd5502f87784c803c9c
pushmessi10 <43080504+pushmessi10@users.noreply.github.com>
2018-11-14 09:49:33 -0600

    Years added 1992

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -124,6 +124,8 @@ id="Address" size=30 maxlength=30></textarea> </td>
         <select name="Year" id="Year"> 
           
         <option value="-1">Year:</option> 
+        <option value="1992">1992</option>
+        <option value="1993">1993</option>       
         <option value="1994">1994</option> 
         <option value="1995">1995</option> 
         <option value="1996">1996</option> 
